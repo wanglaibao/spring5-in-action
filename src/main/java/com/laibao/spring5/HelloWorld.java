1c6eb15a4836105f0ca3a76310f5c17802a9030a
jinge <jinge@wacai.com>
2017-12-29 22:34:36 +0800

    tijiao

---
+++ src/main/java/com/laibao/spring5/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.laibao.spring5;
+
+/**
+ * @author laibao wang
+ * @date 2017-12-29
+ * @version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("this is the hello world java program!");
+    }
+}
