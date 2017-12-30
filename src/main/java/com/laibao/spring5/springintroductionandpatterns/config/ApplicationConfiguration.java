ca8f827450e3a89154cbc34511cf838ebab01a16
jinge <jinge@wacai.com>
2017-12-30 15:55:12 +0800

    update ApplicationConfiguration java info

--- src/main/java/com/laibao/spring5/springintroductionandpatterns/config/ApplicationConfiguration.java
+++ src/main/java/com/laibao/spring5/springintroductionandpatterns/config/ApplicationConfiguration.java
@@ -1,21 +1,21 @@
 package com.laibao.spring5.springintroductionandpatterns.config;
 
+import com.laibao.spring5.springintroductionandpatterns.aspect.LoggingAspect;
 import com.laibao.spring5.springintroductionandpatterns.repository.AccountRepository;
 import com.laibao.spring5.springintroductionandpatterns.repository.TransferRepository;
 import com.laibao.spring5.springintroductionandpatterns.repository.impl.JdbcAccountRepository;
 import com.laibao.spring5.springintroductionandpatterns.repository.impl.JdbcTransferRepository;
 import com.laibao.spring5.springintroductionandpatterns.service.TransferService;
 import com.laibao.spring5.springintroductionandpatterns.service.impl.TransferServiceImpl;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 
 /**
  * @author laibao wang
  * @date 2017-12-30
  * @version 1.0
  */
+
+@EnableAspectJAutoProxy
 @Configuration
 @ComponentScan
 public class ApplicationConfiguration {
@@ -33,5 +33,10 @@ public class ApplicationConfiguration {
 
     public TransferRepository transferRepository() {
         return new JdbcTransferRepository();
+    }
+
+    @Bean
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
     }
 }
