3eddb28f8a4edd5dde52810f93e5bbd8b589672f
jinge <jinge@wacai.com>
2017-12-30 08:54:59 +0800

    add new java files info

---
+++ src/main/java/com/laibao/spring5/hello/BeanConfiguration.java
@@ -0,0 +1,24 @@
+package com.laibao.spring5.hello;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author laibao wang
+ * @date 2017-12-30
+ * @version 1.0
+ */
+@Configuration
+@ComponentScan
+public class BeanConfiguration {
+
+    @Bean
+    MessageService mockMessageService() {
+        return new MessageService() {
+            public String getMessage() {
+                return "Hello World!\n This is the simple bean configuration Spring Hello World Example";
+            }
+        };
+    }
+}
