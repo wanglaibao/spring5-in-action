cb37651570ebada4c06c8900e5471ff7e09d55d2
jinge <jinge@wacai.com>
2017-12-30 10:35:22 +0800

    add TransferMain java file

---
+++ src/main/java/com/laibao/spring5/springintroductionandpatterns/TransferMain.java
@@ -0,0 +1,13 @@
+package com.laibao.spring5.springintroductionandpatterns;
+
+/**
+ * @author laibao wang
+ * @date 2017-12-30
+ * @version 1.0
+ */
+public class TransferMain {
+
+    public static void main(String[] args) {
+
+    }
+}
