fa5bde8e0b2aadf70a6a7763db9d97896a1a0334
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-01-01 17:26:59 +0800

    add new java files info

---
+++ introduction/src/main/java/com/laibao/spring5/introduction/HelloApplication.java
@@ -0,0 +1,34 @@
+package com.laibao.spring5.introduction;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author laibao wang
+ * @date 2017-12-29
+ * @version 1.0
+ */
+
+@Configuration
+@ComponentScan
+public class HelloApplication {
+
+    @Bean
+    MessageService mockMessageService() {
+        return new MessageService() {
+            public String getMessage() {
+                return "Hello World!\n This is the simple Spring Hello World Example";
+            }
+        };
+    }
+
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(HelloApplication.class);
+        MessagePrinter messagePrinter = applicationContext.getBean(MessagePrinter.class);
+        messagePrinter.printMessage();
+    }
+}
---
+++ introduction/src/main/java/com/laibao/spring5/introduction/MessageSetterPrinter.java
@@ -0,0 +1,20 @@
+package com.laibao.spring5.introduction;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author laibao wang
+ * @date 2017-12-30
+ * @version 1.0
+ */
+@Component
+public class MessageSetterPrinter {
+
+    @Autowired
+    private MessageService messageService;
+
+    public void printMessage() {
+        System.out.println(this.messageService.getMessage());
+    }
+}
